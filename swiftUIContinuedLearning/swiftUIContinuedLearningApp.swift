0e545c445c01d06db6ebbfa01b47ec9f8aa4b05c
Brahim tuijine <farahdkhil000@gmail.com>
2024-10-20 23:12:03 +0100

    Initial Commit

---
+++ swiftUIContinuedLearning/swiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIContinuedLearningApp.swift
+//  swiftUIContinuedLearning
+//
+//  Created by MacBook on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
