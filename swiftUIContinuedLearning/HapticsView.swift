cb136e11994d4a9850f3fad11094576f112528c5
Brahim tuijine <brahimtuijine220@gmail.com>
2024-10-23 00:32:22 +0100

    haptics

---
+++ swiftUIContinuedLearning/HapticsView.swift
@@ -0,0 +1,44 @@
+//
+//  HapticsView.swift
+//  swiftUIContinuedLearning
+//
+//  Created by MacBook on 23/10/2024.
+//
+
+import SwiftUI
+
+class HapticsManager {
+    static let instance = HapticsManager()
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) -> Void {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) -> Void {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        
+    }
+}
+
+struct HapticsView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Button("success") {HapticsManager.instance.notification(type: .success)}
+            Button("error") {HapticsManager.instance.notification(type: .error)}
+            Button("warning") {HapticsManager.instance.notification(type: .warning)}
+            
+            Divider()
+
+            Button("soft") {HapticsManager.instance.impact(style: .soft)}
+            Button("light") {HapticsManager.instance.impact(style: .light)}
+            Button("medium") {HapticsManager.instance.impact(style: .medium)}
+            Button("rigid") {HapticsManager.instance.impact(style: .rigid)}
+            Button("heavy") {HapticsManager.instance.impact(style: .heavy)}
+        }
+    }
+}
+
+#Preview {
+    HapticsView()
+}
