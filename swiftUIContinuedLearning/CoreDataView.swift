d241e300d3afc403cb18088660f64ff3f0eb5df8
Brahim tuijine <brahimtuijine220@gmail.com>
2024-10-25 00:11:59 +0100

    create CoreDataViewModel with fetch and add fruits

---
+++ swiftUIContinuedLearning/CoreDataView.swift
@@ -0,0 +1,72 @@
+//
+//  CoreDataView.swift
+//  swiftUIContinuedLearning
+//
+//  Created by MacBook on 24/10/2024.
+//
+
+import SwiftUI
+import CoreData
+
+class CoreDataViewModel: ObservableObject {
+    
+    static let shared : CoreDataViewModel = CoreDataViewModel()
+    
+    @Published var fruits: [FruitsEntity] = []
+    
+    
+    let container: NSPersistentContainer
+    
+    
+    init() {
+        self.container = NSPersistentContainer(name: "FruitsContainer")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("error loading core data \(error.localizedDescription)")
+            } else {
+                print ("Successfully loaded core data!")
+            }
+        }
+        
+        fetchFruits()
+    }
+    
+    func fetchFruits() -> Void {
+        let request = NSFetchRequest<FruitsEntity>(entityName: "FruitsEntity")
+        
+        do {
+            fruits = try container.viewContext.fetch(request)
+        } catch {
+            print("error during fetch data \(error)")
+        }
+    }
+    
+    func addFruit(fruitTitle : String) -> Void {
+        let fruit = FruitsEntity(context: container.viewContext)
+        fruit.name = fruitTitle
+        saveFruit()
+    }
+    
+    private func saveFruit() -> Void {
+        do {
+           try container.viewContext.save()
+            fetchFruits()
+        } catch {
+            print("error when save fruit \(error)")
+        }
+    }
+    
+}
+
+struct CoreDataView: View {
+    
+    @StateObject private var vm : CoreDataViewModel = CoreDataViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataView()
+}
