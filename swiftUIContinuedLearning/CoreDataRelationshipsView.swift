a0171e786a2d0202ae832bd2419c4b7f4ada8e8f
Brahim tuijine <brahimtuijine220@gmail.com>
2024-10-26 15:50:36 +0100

    create core data manager class and link it with viewModel

---
+++ swiftUIContinuedLearning/CoreDataRelationshipsView.swift
@@ -0,0 +1,54 @@
+//
+//  CoreDataRelationshipsView.swift
+//  swiftUIContinuedLearning
+//
+//  Created by MacBook on 25/10/2024.
+//
+
+import SwiftUI
+import CoreData
+
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    init() {
+        self.container = NSPersistentContainer(name: "CoreDataContainer")
+        container.loadPersistentStores { descripton, error in
+            if let error = error {
+                print("error loading data \(error.localizedDescription)")
+            } else {
+                print("load data success")
+            }
+        }
+        context = container.viewContext
+    }
+    
+    func save() -> Void {
+        do {
+            try context.save()
+        } catch {
+            print("error when save data \(error.localizedDescription)")
+        }
+    }
+}
+
+class CoreDataRelationshipsViewModel: ObservableObject {
+    let manager : CoreDataManager = CoreDataManager.instance
+
+}
+
+struct CoreDataRelationshipsView: View {
+    
+    @StateObject var vm = CoreDataRelationshipsViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataRelationshipsView()
+}
