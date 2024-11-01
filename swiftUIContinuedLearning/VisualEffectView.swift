eff7f5487d5bd9aef6a9566787c147c8b0bcfd37
Brahim tuijine <brahimtuijine220@gmail.com>
2024-11-02 00:28:26 +0100

    change view offset using visualEffect

--- swiftUIContinuedLearning/VisualEffectView.swift
+++ swiftUIContinuedLearning/VisualEffectView.swift
@@ -11,24 +11,39 @@ struct VisualEffectView: View {
     @State var width: Double = 100
     
     var body: some View {
-        Text("Hello world")
-            .frame(width: width)
-            .padding()
-            .background(.red)
-            .visualEffect { content, geometryProxy in
-                content.grayscale(geometryProxy.size.width >= 200 ? 1 : 0)
-            }
-            .onTapGesture {
-                withAnimation {
-                    if width == 300 {
-                        width = 100
-                    } else {
-                        width = 300
-                    }
+        
+        ScrollView {
+            
+            VStack(spacing: 20) {
+                ForEach(0..<100) { value in
+                    Rectangle()
+                        .frame(width: 300, height: 300)
+                        .frame(maxWidth: .infinity)
+                        .background(.orange)
+                        .visualEffect { content, geometry in
+                            content.offset(x: geometry.frame(in: .global).minY * 0.5)
+                        }
                 }
             }
-
             
+        }
+//        Text("Hello world")
+//            .frame(width: width)
+//            .padding()
+//            .background(.red)
+//            .visualEffect { content, geometryProxy in
+//                content.grayscale(geometryProxy.size.width >= 200 ? 1 : 0)
+//            }
+//            .onTapGesture {
+//                withAnimation {
+//                    if width == 300 {
+//                        width = 100
+//                    } else {
+//                        width = 300
+//                    }
+//                }
+//            }
+    
     }
 }
 
